37ad7f3bf444a41f9745461e753052944ed2906e
Abhishek <116814896+Intellect145@users.noreply.github.com>
2025-03-15 11:03:12 +0530

    Initial Commit

---
+++ movies/src/test/java/Movies/Abhi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package Movies.Abhi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
