70a58f68a467aba0cbf6929c72d3ebba93617c8e
Abhishek <abhishekkadagala654@gmail.com>
2025-03-16 12:11:18 +0530

    Retrieved the Data in json format. New reviews had been added that can be seen in Postman.

--- movies/src/main/java/Movies/Abhi/movies/Reviews.java
+++ movies/src/main/java/Movies/Abhi/movies/Reviews.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="reviews")
@@ -13,6 +14,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 
 public class Reviews {
+
+    @Id
     private ObjectId id;
     private String body;
+
+    public Reviews(String body) {
+        this.body = body;
+    }
 }
